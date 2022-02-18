e9c19d46bdbec90d2eb3d4bf5a8559a6b9ce5deb
Mivisha <90041857+Mivisha@users.noreply.github.com>
2022-02-18 20:03:09 +0200

    Add forEach

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-class MyArray{
-    constructor(...item){
-        for(let i=0 ; i<item.length;i++){
-            this[i]=item[i];
-        }
-        this.length=item.length;
+class MyArray {
+  constructor(...item) {
+    for (let i = 0; i < item.length; i++) {
+      this[i] = item[i];
     }
+    this.length = item.length;
+  }
 
-    push(...item){
-        for(let i=this.length,j=0; j<item.length;i++){
-            this[i]=item[j++];
-        }
-        this.length=this.length+item.length;
-        return this;
+  push(...item) {
+    for (let i = this.length, j = 0; j < item.length; i++) {
+      this[i] = item[j++];
     }
-    pop(){
-        let item=this[--this.length];
-        delete this[this.length];
-        
-        return item;
-    }
+    this.length = this.length + item.length;
+    return this;
+  }
+  pop() {
+    let item = this[--this.length];
+    delete this[this.length];
 
-    map(fun){
-        const arr = new MyArray();
-        for (let i = 0; i < this.length; i++) {
-            arr.push(fun(this[i]));
-        }
-        return arr;
+    return item;
+  }
+  forEach(fun) {
+    for (let i = 0; i < this.length; i++) {
+      fun(this[i]);
     }
+    return undefined;
+  }
+  map(fun) {
+    const arr = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      arr.push(fun(this[i]));
+    }
+    return arr;
+  }
 }
-const arr = new MyArray(1,2,3,4);
+const arr = new MyArray(1, 2, 3, 4);
