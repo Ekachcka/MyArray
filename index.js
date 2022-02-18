313c52aaf6c605ab57da64732cc64f7d2eca1866
Mivisha <90041857+Mivisha@users.noreply.github.com>
2022-02-18 19:00:12 +0200

    Add push

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class MyArray{
-    
+    constructor(...item){
+        for(let i=0 ; i<item.length;i++){
+            this[i]=item[i];
+        }
+        this.length=item.length;
+    }
+
+    push(...item){
+        for(let i=this.length,j=0; j<item.length;i++){
+            this[i]=item[j++];
+        }
+        this.length=this.length+item.length;
+        return this;
+    }
 }
+const arr = new MyArray(1,2,3,4);
